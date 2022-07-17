a2846798886c350cf525933e950ea41b6e24a6c9
potlitel <potlitel@gmail.com>
2022-07-17 15:07:21 -0400

    first commit

---
+++ Models/SuperheroItem.cs
@@ -0,0 +1,12 @@
+namespace Superhero
+{
+    public class SuperheroItem
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? publisher { get; set; }
+        public string? alter_ego { get; set; }
+        public string? first_appearance { get; set; }
+        public string? characters { get; set; }
+    }
+}
